8378ab1171986153b16a02950a9a9ba3fed1fb99
Alejandro Soret <soret_40@hotmail.com>
2014-08-22 17:45:34 +0200

    First version of the main ListView.

--- ASMCameraMinus_Android/src/com/alejandrosoret/asmcameraminus_android/MainActivity.java
+++ ASMCameraMinus_Android/src/com/alejandrosoret/asmcameraminus_android/MainActivity.java
@@ -1,40 +1,53 @@
 package com.alejandrosoret.asmcameraminus_android;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+/*************************************************************/
+/*                                                           */ 
+/* MainActivity                                              */ 
+/* (c)2014 Alejandro                                         */ 
+/*                                                           */ 
+/* Description: MainActivity Class                           */ 
+/*              ASMCameraMinus_Android Project               */ 
+/*                                                           */ 
+/*                                                           */ 
+/*************************************************************/
 public class MainActivity extends ActionBarActivity
 {
-
+	
+	private ListView mainListView;
+	private ArrayAdapter< String > listAdapter;
+	
+	/*********************************************************/
+	/*                                                       */ 
+	/* MainActivity.onCreate()                               */ 
+	/*                                                       */ 
+	/*********************************************************/
 	@Override
 	protected void onCreate( Bundle savedInstanceState )
 	{
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.activity_main );
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu( Menu menu )
-	{
-		// Inflate the menu; this adds items to the action bar if it is
-		// present.
-		getMenuInflater().inflate( R.menu.main, menu );
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected( MenuItem item )
-	{
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if( id == R.id.action_settings )
-		{
-			return true;
-		}
-		return super.onOptionsItemSelected( item );
-	}
+		
+		mainListView = (ListView) findViewById( R.id.mainListView );
+		
+		// Create and populate a List of names. 
+		String[] names = new String[] { "Alejandro", "Jorge", "Yvon", "Julian", "David", "Pablo", "Jose", "Jon", "Kofi" };
+		ArrayList< String > nameList = new ArrayList<String>();
+		nameList.addAll( Arrays.asList( names ) );
+		
+		// Create ArrayAdapter using the names list.
+		listAdapter = new ArrayAdapter<String>( this, R.layout.listview_main_item, nameList );
+		
+		// Set the ArrayAdapter as the ListView's adapter.
+		mainListView.setAdapter( listAdapter );
+	}	
 }
